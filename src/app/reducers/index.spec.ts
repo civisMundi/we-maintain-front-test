a469fb50244b6a97631c2a1a9dc146405608a9f2
dydyFos <dylanfoster.esgi@gmail.com>
2018-05-29 23:14:07 +0200

    Tests fixed
    -- reducers coverage

---
+++ src/app/reducers/index.spec.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { async } from "@angular/core/testing";
+import { appReducer, AppState } from "./index";
+import { defaultMessagesState } from "./messages/messages.reducer";
+import { defaultUserState } from "./user/user.reducer";
+import { defaultChannelsState } from "./channels/channels.reducer";
+import { defaultNotificationsState } from "./notifications/notifications.reducer";
+import { Action } from "../typings/Action";
+
+export const defaultState: AppState = {
+    messages: defaultMessagesState,
+    user: defaultUserState,
+    channels: defaultChannelsState,
+    notifs: defaultNotificationsState,
+};
+
+describe("Main Reducer - index", () => {
+    beforeEach(async(() => {
+    }));
+
+    it("should return default state", async(() => {
+        const action: Action = {
+            type: "whocares", payload: "yomoma"
+        };
+        expect(appReducer(undefined, action)).toEqual(defaultState);
+    }));
+});
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,8 +7,8 @@ module.exports = function (config) {
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
-      // require("karma-phantomjs-launcher"),
-      require("karma-firefox-launcher"),
+      require("karma-phantomjs-launcher"),
+      // require("karma-firefox-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage-istanbul-reporter"),
       require("@angular-devkit/build-angular/plugins/karma")
@@ -28,8 +28,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ["PhantomJS"],
-    browsers: ["Firefox"],
+    browsers: ["PhantomJS"],
+    // browsers: ["Firefox"],
     singleRun: false
   });
 };
